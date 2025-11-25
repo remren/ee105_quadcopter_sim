551073d610a2395c238b2eb3d82c8e7580fc2d9b
remren <remren>
2025-11-26 00:43:58 +0100

    more simulink

---
+++ workspaces/remy/test_plot_carryover.m
@@ -0,0 +1,8 @@
+% Rotational Matrix
+    c_phi = cos(phi); s_phi = sin(phi);
+    c_theta = cos(theta); s_theta = sin(theta);
+    c_psi = cos(psi); s_psi = sin(psi);
+
+    R = [c_psi*c_theta, c_psi*s_theta*s_phi - s_psi*c_phi, c_psi*s_theta*c_phi + s_psi*s_phi;
+         s_psi*c_theta, s_psi*s_theta*s_phi + c_psi*c_phi, s_psi*s_theta*c_phi - c_psi*s_phi;
+         -s_theta,      c_theta*s_phi,                   c_theta*c_phi];
